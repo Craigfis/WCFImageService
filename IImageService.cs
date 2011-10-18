15bc9a9417089ceaa421fc2fd35178d79743b721
Craig Fisher <bzc@hotmail.com>
2011-10-17 23:12:33 -0700

    Initial code

---
+++ IImageService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfImageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IImageService" in both code and config file together.
+    [ServiceContract]
+    public interface IImageService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [WebGet(ResponseFormat=WebMessageFormat.Xml)]
+        string GetString();
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        CompositeType GetAnImage();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "GetImage")]
+        //[WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        System.ServiceModel.Channels.Message GetImage();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+        byte[] image;
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+
+        [DataMember]
+        public byte[] Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+    }
+}
---
+++ ImageService.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+
+namespace WcfImageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ImageService" in code, svc and config file together.
+    public class ImageService : IImageService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public string GetString()
+        {
+            WebOperationContext.Current.OutgoingResponse.Headers.Add("Cache-Control", "no-cache"); 
+            return "Playing with WCF";
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public CompositeType GetAnImage()
+        {
+            WebOperationContext.Current.OutgoingResponse.Headers.Add("Cache-Control", "no-cache");
+            var result = new CompositeType();
+            result.StringValue = "Here's a pic for ya";
+            var f = File.OpenRead(@"C:\Users\Consultant4\Pictures\2011-09-18 AnnetteLake\AnnetteLake 006.jpg");
+            byte[] buffer = new byte[f.Length];
+            f.Read(buffer, 0, (int)f.Length);
+            result.Image = buffer;
+
+            return result;
+        }
+
+        public System.ServiceModel.Channels.Message GetImage()
+        {
+            var files = Directory.GetFiles(@"C:\Users\Consultant4\Pictures", "*.jpg", SearchOption.AllDirectories);
+            var rnd = new Random(DateTime.Now.Millisecond);
+            var fnum = rnd.Next(files.Length);
+            var filename = files[fnum];
+            var f = File.OpenRead(filename);
+            //            var f = File.OpenRead(@"C:\Users\Consultant4\Pictures\2011-09-18 AnnetteLake\AnnetteLake 006.jpg");
+            byte[] buffer = new byte[f.Length];
+            f.Read(buffer, 0, (int)f.Length);
+            var ms = new MemoryStream(buffer);
+            return WebOperationContext.Current.CreateStreamResponse(ms, "image/jpeg");
+        }
+    }
+}
